da7e580d37506da1617caa03758a2c1f7d3941cc
Francois-Xavier Cat <fxcat@lazywinadmin.com>
2019-02-10 21:13:22 -0800

    Add support to deploy to PSGallery

--- build.ps1
+++ build.ps1
@@ -1,7 +1,29 @@
-# Install Pester
-Install-Module -Name Pester
+Try {
+    # Install Pester
+    Write-Host "Installing Pester..." -ForegroundColor Cyan
+    Install-Module -Name Pester
 
-# Run test build
-$res = Invoke-Pester -Path ".\Tests" -OutputFormat NUnitXml -OutputFile TestsResults.xml -PassThru
-(New-Object 'System.Net.WebClient').UploadFile("https://ci.appveyor.com/api/testresults/nunit/$($env:APPVEYOR_JOB_ID)", (Resolve-Path .\TestsResults.xml))
-if ($res.FailedCount -gt 0) { throw "$($res.FailedCount) tests failed."}
+    # Run test build
+    Write-Host "Running Tests..." -ForegroundColor Cyan
+    $res = Invoke-Pester -Path ".\Tests" -OutputFormat NUnitXml -OutputFile TestsResults.xml -PassThru
+    Write-Host "Publish tests..." -ForegroundColor Cyan
+    (New-Object 'System.Net.WebClient').UploadFile("https://ci.appveyor.com/api/testresults/nunit/$($env:APPVEYOR_JOB_ID)", (Resolve-Path .\TestsResults.xml))
+    if ($res.FailedCount -gt 0) { throw "$($res.FailedCount) tests failed."}
+
+
+
+    # Publish to Gallery
+    if ($APPVEYOR_REPO_COMMIT_MESSAGE -match '!release'){
+        $Splatting = @{
+            Path        = '.\PSModuleSample'
+            NuGetApiKey = $env:NuGetApiKey
+            ErrorAction = 'Stop'
+        }
+        Write-Host "Publishing to the PowerShell Gallery." -ForegroundColor Cyan
+        Publish-Module @Splatting
+    }
+}
+Catch
+{
+    throw $_
+}
